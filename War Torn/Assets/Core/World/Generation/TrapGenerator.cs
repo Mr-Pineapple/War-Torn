5606309c369dc27aa1b9031a2b0d955d708e7a5b
Mr-Pineapple <44590089+Mr-Pineapple@users.noreply.github.com>
2024-04-18 04:39:57 +0100

    :construction: Changed range variables
    Removed min max ranges as they would be the same values just positive and negative

--- War Torn/Assets/Core/World/Generation/TrapGenerator.cs
+++ War Torn/Assets/Core/World/Generation/TrapGenerator.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class TrapGenerator : MonoBehaviour {
     [SerializeField] GameObject trap;
-    [SerializeField] float minRangeX, maxRangeX, minRangeZ, maxRangeZ;
+    [SerializeField] float xRange, zRange;
 
     private void Start() {
-        Vector3 randomPosition = new Vector3(gameObject.transform.localPosition.x + Random.Range(maxRangeX, minRangeX), 0, gameObject.transform.localPosition.z + Random.Range(maxRangeZ, minRangeZ));
+        Vector3 randomPosition = new Vector3(gameObject.transform.localPosition.x + Random.Range(-xRange, xRange), 0, gameObject.transform.localPosition.z + Random.Range(-zRange, zRange));
         Instantiate(trap, randomPosition, gameObject.transform.rotation * Quaternion.identity * gameObject.transform.localRotation);
     }
 }
